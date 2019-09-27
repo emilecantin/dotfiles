0aea2502a1b6487c119978da580a2921df6c7070
Emile Cantin <emile.cantin@gmail.com>
2019-09-27 16:01:59 -0400

    Fix nvim symlink

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -s .dotfiles/vim/.vimrc
 ln -s .dotfiles/tmux/.tmux.conf
 ln -s .dotfiles/.tmux.conf.local
 mkdir -p .config
-ln -s ~/.dotfiles/vim .config/nvim
+ln -s ~/.dotfiles/nvim .config
 popd
 
 git submodule init
